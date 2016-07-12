ecba805bdf1f0a7785df21f555165e952cd643b1
YoungRichNigger9 <francoistranchot@gmail.com>
2016-07-12 17:09:33 +0200

    Update plugin.php

--- src/BoxTool/plugin.php
+++ src/BoxTool/plugin.php
@@ -14,6 +14,11 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\permission\Permission;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerGameModeChangeEvent;
 
 class plugin extends PluginBase{
 
@@ -26,4 +31,8 @@ class plugin extends PluginBase{
           public function onDisable(){
                     $this->getLogger()->info(TextFormat:: RED."BoxTool as Disabled");
           }
+          public function onGameModeChange(PlayerGameModeChangeEvent $event){
+		$player = $event->getPlayer();
+		$name = $player->getName();
+		$this->getServer()->broadcastPopup(Colour::White."$name".Colour::DARK_GREEN."Changed Gamemode")
 }
