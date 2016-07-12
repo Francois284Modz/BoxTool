9112ac02c34e98084c9c7d8aee86a25e8f38a592
YoungRichNigger9 <francoistranchot@gmail.com>
2016-07-12 16:35:02 +0200

    1st php file ^_-

--- src/BoxTool/plugin.php
+++ src/BoxTool/plugin.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace BoxTool\plugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase{
+
+          public function onLoad(){
+                    $this->getLogger()->info(TextFormat::BLUE."BoxTool is  Loading");
+          }
+          public function onEnable(){
+                    $this->getLogger()->info(TextFormat::GREEN."BoxTool as Enabled");
+          }
+          public function onDisable(){
+                    $this->getLogger()->info(TextFormat:: RED."BoxTool as Disabled");
+          }
+}
